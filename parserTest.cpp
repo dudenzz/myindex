dba3c9d822f2799da264f73f2ee823a9693cf20f
dudenzz <kubadt@gmail.com>
2019-06-05 15:35:27 +0200

    some rms

--- parserTest.cpp
+++
@@ -1,6 +0,0 @@
-#include "pmXMLparser.h"
-int main(int argc, char **argv)
-{
-	PmXMLparser parser = PmXMLparser();
-	parser.ParsePath(argv[1], atoi(argv[2]));
-}
--- test_wordmap.cpp
+++
@@ -1,6 +0,0 @@
-#include "wordmap.h"
-
-int main()
-{
-
-}
